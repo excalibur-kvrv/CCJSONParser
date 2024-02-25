0b24042a8f3612a7633755dcdaa2487c0bf81412
excalibur-kvrv <excalibur.krv@gmail.com>
2024-02-26 01:44:29 +0530

    added directory specifying option for running tests

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,26 @@
+import java.io.File;
+import java.util.Arrays;
+import java.util.Objects;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Lexer lex = new Lexer("");
-        Parser parser = new Parser(lex);
-        try {
-            if (parser.parseJSON()) {
-                System.out.println("Valid JSON structure");
-            } else {
+        // Provide tests directory as cli argument
+        File file = new File(args[0]);
+        for (String fileName: Objects.requireNonNull(file.list())) {
+            System.out.println(fileName);
+            try {
+                Lexer lex = new Lexer(args[0] + fileName);
+                Parser parser = new Parser(lex);
+                if (parser.parseJSON()) {
+                    System.out.println("Valid JSON structure");
+                } else {
+                    System.out.println("Invalid JSON structure");
+                }
+            } catch (Exception e) {
                 System.out.println("Invalid JSON structure");
             }
-        } catch (Exception e) {
-            System.out.println("Invalid JSON structure");
         }
     }
 }
--- src/Parser.java
+++ src/Parser.java
@@ -6,12 +6,11 @@ public class Parser {
     }
 
     private boolean parseRaw(String token) {
-        System.out.println("Parsing raw " + token);
         for (int i = 0; i < token.length(); i++) {
             if (lexer.peek(1).token() != token.charAt(i)) {
                 return false;
             } else {
-                System.out.println(lexer.getNextToken());
+                lexer.getNextToken();
             }
         }
         return true;
@@ -19,13 +18,11 @@ public class Parser {
 
     public boolean parseJSON() {
         // json -> element
-        System.out.println("Parsing JSON");
         return parseElement();
     }
 
     private boolean parseValue() {
         // value -> object | array | string | number | 'true' | 'false' | 'null'
-        System.out.println("Parsing Value");
         Token currentToken = lexer.peek(1);
         boolean match = switch (currentToken.tokenType()) {
             case LEFT_CURLY_BRACE -> parseObject();
@@ -42,8 +39,7 @@ public class Parser {
 
     private boolean parseObject() {
         // object -> '{' ws '}' | '{' members '}'
-        System.out.println("Parsing Object");
-        System.out.println(lexer.getNextToken());
+        lexer.getNextToken();
         boolean match;
         match = parseMembers() || parseWhiteSpace();
         return match && parseRaw("}");
@@ -51,45 +47,39 @@ public class Parser {
 
     private boolean parseMembers() {
         // members -> member | member ',' members
-        System.out.println("Parsing members");
         boolean match;
         match = parseMember();
         Token currentToken = lexer.peek(1);
         char token = currentToken.token();
         if (token == ',') {
-            System.out.println(lexer.getNextToken());
+            lexer.getNextToken();
             return match && parseMembers();
         }
         return match;
     }
 
     private boolean parseMember() {
-        System.out.println("Parsing member");
         // member -> ws string ws ':' element
         return parseWhiteSpace() && parseString() && parseWhiteSpace() && lexer.getNextToken().token() == ':' && parseElement();
     }
 
     private boolean parseArray() {
         // array -> '[' ws ']' | '[' elements ']'
-        System.out.println("Parsing array");
         Token token = lexer.getNextToken();
-        System.out.println(token);
         boolean match = token.token() == '[';
         match = match && (parseElements() || parseWhiteSpace());
         token = lexer.getNextToken();
-        System.out.println(token);
         match = match && token.token() == ']';
         return match;
     }
 
     private boolean parseElements() {
         // elements -> element | element ',' elements
-        System.out.println("Parsing elements");
         boolean match = parseElement();
         Token currentToken = lexer.peek(1);
         char token = currentToken.token();
         if (token == ',') {
-            System.out.println(lexer.getNextToken());
+            lexer.getNextToken();
             return match && parseElements();
         }
         return match;
@@ -97,41 +87,34 @@ public class Parser {
 
     private boolean parseElement() {
         // element -> ws value ws
-        System.out.println("Parsing element");
         return parseWhiteSpace() && parseValue() && parseWhiteSpace();
     }
 
     private boolean parseString() {
         // string -> '"' characters '"'
-        System.out.println("Parsing string");
         Token token = lexer.getNextToken();
-        System.out.println(token);
         boolean match = token.token() == '"';
         match = match && parseCharacters();
         token = lexer.getNextToken();
-        System.out.println(token);
         match = match && token.token() == '"';
         return match;
     }
 
     private boolean parseCharacters() {
         // characters -> "" | character characters
-        System.out.println("Parsing characters");
         if (parseCharacter()) {
-            System.out.println(lexer.getNextToken());
+            lexer.getNextToken();
             parseCharacters();
-//            return true;
         }
         return true;
     }
 
     private boolean parseCharacter() {
         // character -> 'U+0020' . 'U+10FFFF' - '"' - '\' | '\' escape
-        System.out.println("Parsing characters");
         Token currentToken = lexer.peek(1);
         char token = currentToken.token();
         if (token == '\\') {
-            System.out.println(lexer.getNextToken());
+            lexer.getNextToken();
             return parseEscape();
         } else if (token == '"') {
             return false;
@@ -141,16 +124,15 @@ public class Parser {
 
     private boolean parseEscape() {
         // escape -> '"' | '\' | '/' | 'b' | 'f' | 'n' | 'r' | 't' | 'u' hex hex hex hex
-        System.out.println("Parsing escape");
         Token currentToken = lexer.peek(1);
         char token = currentToken.token();
         return switch (token) {
             case '"', '\\', '/', 'b', 'f', 'n', 'r', 't' -> {
-                System.out.println(lexer.getNextToken());
+                lexer.getNextToken();
                 yield true;
             }
             case 'u' -> {
-                System.out.println(lexer.getNextToken());
+                lexer.getNextToken();
                 yield parseHex() && parseHex() && parseHex() && parseHex();
             }
             default -> false;
@@ -159,12 +141,11 @@ public class Parser {
 
     private boolean parseHex() {
         // hex -> digit | 'A' . 'F' | 'a' . 'f'
-        System.out.println("Parsing hex");
         Token currentToken = lexer.peek(1);
         int token = currentToken.token();
         return parseDigit() || switch (token) {
             case 65, 66, 67, 68, 69, 70, 97, 98, 99, 100, 101, 102 -> {
-                System.out.println(lexer.getNextToken());
+                lexer.getNextToken();
                 yield true;
             }
             default -> false;
@@ -173,17 +154,15 @@ public class Parser {
 
     private boolean parseNumber() {
         // number -> integer fraction exponent
-        System.out.println("Parsing number");
         return parseInteger() && parseFraction() && parseExponent();
     }
 
     private boolean parseInteger() {
         // integer -> digit | onenine digits | '-' digit | '-' onenine digits
-        System.out.println("Parsing integer");
         Token currentToken = lexer.peek(1);
         char current = currentToken.token();
         if (current == '-') {
-            System.out.println(lexer.getNextToken());
+            lexer.getNextToken();
             return (parseOneNine() && parseDigits()) || parseDigit();
         }
         return (parseOneNine() && parseDigits()) || parseDigit();
@@ -191,7 +170,6 @@ public class Parser {
 
     private boolean parseDigits() {
         // digits -> digit | digit digits
-        System.out.println("Parsing digits");
         boolean match = parseDigit();
         Token currentToken = lexer.peek(1);
         char token = currentToken.token();
@@ -205,11 +183,10 @@ public class Parser {
 
     private boolean parseDigit() {
         // digit -> '0' | onenine
-        System.out.println("Parsing digit");
         Token currentToken = lexer.peek(1);
         char current = currentToken.token();
         if (current == '0') {
-            System.out.println(lexer.getNextToken());
+            lexer.getNextToken();
             return true;
         }
         return parseOneNine();
@@ -217,12 +194,11 @@ public class Parser {
 
     private boolean parseOneNine() {
         // onenine -> '1' . '9'
-        System.out.println("Parsing one nine");
         Token currentToken = lexer.peek(1);
         char current = currentToken.token();
         return switch (current) {
             case '1', '2', '3', '4', '5', '6', '7', '8', '9' -> {
-                System.out.println(lexer.getNextToken());
+                lexer.getNextToken();
                 yield true;
             }
             default -> false;
@@ -231,11 +207,10 @@ public class Parser {
 
     private boolean parseFraction() {
         // fraction -> '' | '.' digits
-        System.out.println("Parsing fraction");
         Token currentToken = lexer.peek(1);
         char token = currentToken.token();
         if (token == '.') {
-            System.out.println(lexer.getNextToken());
+            lexer.getNextToken();
             return parseDigits();
         }
         return true;
@@ -243,11 +218,10 @@ public class Parser {
 
     private boolean parseExponent() {
         // exponent -> '' | 'E' sign digits | 'e' sign digits
-        System.out.println("Parsing exponent");
         Token currentToken = lexer.peek(1);
         char token = currentToken.token();
         if (token == 'E' || token == 'e') {
-            System.out.println(lexer.getNextToken());
+            lexer.getNextToken();
             return parseSign() && parseDigits();
         }
         return true;
@@ -255,23 +229,21 @@ public class Parser {
 
     private boolean parseSign() {
         // sign -> '' | '+' | '-'
-        System.out.println("Parsing sign");
         Token currentToken = lexer.peek(1);
         char token = currentToken.token();
         if (token == '+' || token == '-') {
-            System.out.println(lexer.getNextToken());
+            lexer.getNextToken();
         }
         return true;
     }
 
     private boolean parseWhiteSpace() {
         // ws -> '' | ' ' ws | '\n' ws | '\r' ws | '\t' ws
-        System.out.println("Parsing whitespace");
         Token currentToken = lexer.peek(1);
         if (currentToken == null) return true;
         char token = currentToken.token();
         if (token == ' ' || token == '\n' || token == '\r' || token == '\t') {
-            System.out.println(lexer.getNextToken());
+            lexer.getNextToken();
             return parseWhiteSpace();
         }
         return true;
